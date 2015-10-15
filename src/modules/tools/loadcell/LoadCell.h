461616e2ca1f05bea7259e8fa0a8112f56c638ce
Alexandre Kaspar <akaspar@mit.edu>
2015-10-14 23:40:04 -0400

    Imported HX711 code into LoadCell

--- src/modules/tools/loadcell/LoadCell.h
+++ src/modules/tools/loadcell/LoadCell.h
@@ -11,46 +11,34 @@
 
 class LoadCell : public Module{
     public:
-        Endstops();
+        LoadCell();
         void on_module_loaded();
         void on_gcode_received(void* argument);
         void on_config_reload(void* argument);
-        uint32_t acceleration_tick(uint32_t dummy);
 
     private:
-        void home(char axes_to_move);
-        void do_homing_cartesian(char axes_to_move);
-        void do_homing_corexy(char axes_to_move);
-        void wait_for_homed(char axes_to_move);
-        void wait_for_homed_corexy(int axis);
-        void corexy_home(int home_axis, bool dirx, bool diry, float fast_rate, float slow_rate, unsigned int retract_steps);
-        void back_off_home(char axes_to_move);
-        void move_to_origin(char);
+        void set_gain(char gain, bool apply = true);
+        bool is_ready();
+        long read();
+        long read_average(char times = 10);
+        float get_value(char times = 1);
+        float get_units(char times = 1);
+        void tare(char times = 10);
+        void set_scale(float scale = 1.f);
+        void set_offset(long offset = 0);
+        void power_down();
+        void power_up();
+
+
         void on_get_public_data(void* argument);
         void on_set_public_data(void* argument);
         void on_idle(void *argument);
 
-        float homing_position[3];
-        float home_offset[3];
-        uint8_t homing_order;
-        std::bitset<3> home_direction;
-        std::bitset<3> limit_enable;
-
-        unsigned int  debounce_count;
-        float  retract_mm[3];
-        float  trim_mm[3];
-        float  fast_rates[3];
-        float  slow_rates[3];
-        Pin    pins[6];
-        volatile float feed_rate[3];
-        struct {
-            bool is_corexy:1;
-            bool is_delta:1;
-            bool is_scara:1;
-            bool move_to_origin_after_home:1;
-            uint8_t bounce_cnt:4;
-            volatile char status:3;
-        };
+        char   gain;
+        long   offset;
+        float  scale;
+        float  last;
+        Pin    pins[2];
 };
 
 #endif
